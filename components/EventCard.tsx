55a525e359bdca37ed987f6a992a18594089ed40
kenenisa1 <kenenisamiesob@gmail.com>
2026-04-02 14:49:03 +0300

    feat: Add EventCard component

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { FaCalendar, FaClock, FaSearchLocation } from 'react-icons/fa';
 
 interface Props {
 title: string;
@@ -14,8 +15,22 @@ const EventCard = ({title,image, slug, location, date, time}: Props) => {
   return (
     <div>
         <Link href={`/events/${slug}}`} id='event-card'>
-            <Image src="/icons/pin.svg" alt='location' width={14} height={14}/>
-            <p>{title}</p>
+            <Image src={image} alt={title} width={410} height={300}/>
+            <div>
+              <FaSearchLocation className='inline-block mr-1'/>
+              <p>{location}</p>
+            </div>
+            <p className='title'>{title}</p>
+            <div>
+              <div>
+                <FaCalendar className='inline-block mr-1'/>
+                <p>{date} </p>
+              </div>
+              <div>
+                <FaClock className='inline-block mr-1'/>
+                <p>{time} </p>
+              </div>
+            </div>
         </Link>
     </div>
   )
