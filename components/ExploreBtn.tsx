fa49664beb5e7cfab5a31ad5111231b15873ec7b
kenenisa1 <kenenisamiesob@gmail.com>
2026-04-02 14:49:47 +0300

    Add styling to ExploreBtn component using tailwindcss

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,10 +3,14 @@
 const ExploreBtn = () => {
   return (
     <div>
-        <button className='bg-indigo-500 font-bold text-xl mt-5 p-4 rounded-xl w-full cursor-pointer'
-        onClick={()=> console.log('CLICK')}>
-
-            Explore Events
+        <button
+         className='bg-indigo-500 font-bold text-xl p-4 rounded-xl w-full cursor-pointer mx-auto mt-7'
+        onClick={()=> console.log('CLICK')}
+        type="button"
+        id="explore-btn">
+          <a href="#events" className="text-white">
+             Explore Events
+          </a>          
         </button>
        
     </div>
