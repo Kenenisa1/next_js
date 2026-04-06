e90727fc5e57dd4a8dfdc37ee4188c26f40252fe
kenenisa1 <kenenisamiesob@gmail.com>
2026-04-06 21:48:07 +0300

    Add route file for API with Post method

---
+++ app/api/events/route.ts
@@ -0,0 +1,50 @@
+import connectToDatabase from "@/lib/mongodb";
+import { v2 as cloudinary } from "cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+import Event from "@/database/event.model"
+
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function POST ( req: NextRequest ) {
+    try {
+        await connectToDatabase();
+        const formData = await req.formData();
+        let event;
+
+        try{
+            event = Object.fromEntries(formData.entries());
+        }
+        catch(e) {
+            return NextResponse.json({message: 'Invalid JSON data format'}, {status: 400});
+
+        }
+        const file = formData.get('image') as File ;
+        if(!file) {
+            return NextResponse.json({message: 'Image file is required'}, {status: 400});
+        }
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const uploadResult = await new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream( {resource_type: 'image', folder: 'events' }, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            }).end(buffer)
+ 
+        })
+        event.image = (uploadResult as {secure_url: string}).secure_url;
+        const createdEvent = await Event.create(event);
+        return NextResponse.json({message: 'Event created successfully', event: createdEvent}, {status: 201})
+    }
+    catch(error) {
+        console.error(error);
+        return NextResponse.json({message: 'Event creation failed', error: error instanceof Error? error.message: 'unknown'}, {status: 500})
+    }
+}
